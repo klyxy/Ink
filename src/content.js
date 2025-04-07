839716331bd71c2b71b54d5fa153b41a4a9ede3b
klyxy <eugenewestmond@gmail.com>
2025-04-07 10:51:44 -0500

    modified:   manifest.json modified:   src/content.js 	modified:   src/styles.css

--- src/content.js
+++ src/content.js
@@ -1,3 +1,5 @@
+//    "matches": ["https://app.schoology.com/*"],
+
 /* Opacities */
 const opacities = {
   1: "1A",
@@ -24,3 +26,56 @@ chrome.storage.local.get(["hex", "opacity", "border"], function (result) {
   );
   document.documentElement.style.setProperty("--ink-border", result.border);
 });
+
+// Compare Versions
+function compare(version1, version2) {
+  const v1 = String(version1).split('.').map(Number);
+  const v2 = String(version2).split('.').map(Number);
+  console.log(`${v1}, ${v2}`)
+
+  for (let i = 0; i < 3; i++) {
+    if (v1[i] > v2[i]) {
+      return 1;
+    }
+    if (v1[i] < v2[i]) {
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
+// Check Version
+if (window.location.href.includes('home')) {
+  (async () => {
+    try {
+      const response = await fetch("https://api.github.com/repos/klyxy/Ink/releases/latest");
+      if (!response.ok) throw new Error(`GitHub API error: ${response.status}`);
+
+      const releaseData = await response.json();
+
+      const result = compare(releaseData.tag_name, chrome.runtime.getManifest().version)
+
+      const banner = document.createElement("div");
+      banner.className = "grading-period-warning messages warning";
+      banner.id = "ink-banner"
+
+      if (result > 0) {
+        console.warn("Outdated");
+        banner.innerHTML = 'You are using an outdated version of Ink. Please update <a href="https://github.com/klyxy/Ink/releases/latest" target="_blank">here</a>';
+      } else if (result < 0) {
+        console.log("Unpublished version")
+        banner.innerText = "You are using an unpublished/development version of Ink";
+      } else {
+        console.log("Up to date");
+      }
+
+      if (result != 0) {
+        document.querySelector("#body").insertBefore(banner, document.querySelector("#body").querySelector("#wrapper"));
+      }
+
+    } catch (error) {
+      console.error("Github Error: ", error);
+    }
+  })();
+};
